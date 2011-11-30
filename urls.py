2e190d02c38a7765dd4f8f8408622de99713a032
Jason Gonzales <jason.gonzales23@gmail.com>
2011-11-29 20:34:40 -0500

    added django cms

---
+++ urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'cpi.views.home', name='home'),
+    # url(r'^cpi/', include('cpi.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+)
+
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+) + urlpatterns
