32c72161e136d34dc79cceae9296688f4e9b2075
Jason Gonzales <jason.gonzales23@gmail.com>
2012-01-15 21:31:17 -0500

    add store locator urls

--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^store-locator/', include('store_locator.urls')),
     url(r'^', include('cms.urls')),
 )
 
